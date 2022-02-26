c9ef5cad6d8d4c62b1969110206160b3ef6e9e94
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-02-26 10:33:19 -0300

    Preparando ambiente de pastas

---
+++ src/pages/Cesta.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Cesta(props) {
+    return(
+        <View>
+            <Text>CESTA</Text>
+        </View>
+    )
+}
