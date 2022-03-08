12962c60b2d6ad9a018b72208e7f0100aca94b7d
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-03-08 12:57:05 -0300

    Adicionando uma lista de items e um scroll view

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,11 +1,5 @@
 import logo from '../assets/logo.png';
 
-// import tomate from '../../assets/frutas/Tomate.png';
-// import brocolis from '../../assets/frutas/Brócolis.png';
-// import batata from '../../assets/frutas/Batata.png';
-// import pepino from '../../assets/frutas/Pepino.png';
-// import abobora from '../../assets/frutas/Abóbora.png';
-
 const cesta = {
   topo: {
     titulo: "Detalhe da cesta",
@@ -23,23 +17,18 @@ const cesta = {
     lista: [
       {
         nome: "Tomate",
-        // imagem: tomate,
       },
       {
         nome: "Brócolis",
-        // imagem: brocolis,
       },
       {
         nome: "Batata",
-        // imagem: batata,
       },
       {
         nome: "Pepino",
-        // imagem: pepino,
       },
       {
         nome: "Abóbora",
-        // imagem: abobora,
       }
     ]
   }
